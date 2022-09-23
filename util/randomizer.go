ac77f1c86a74d73768dbdca946fe5f9978fcc276
onetrillion <tradgardsmastaren@protonmail.com>
2022-09-23 15:11:07 +0200

    add random name function

--- util/randomizer.go
+++ util/randomizer.go
@@ -14,3 +14,15 @@ func RandomRpsChoice() int64 {
 	rpsChoice := rand.Intn(max-min+1) + min
 	return int64(rpsChoice)
 }
+
+// Returns a random name from an array
+func RandomName() string {
+	names := [...]string{
+		"John", "Steve", "Amanda", "Benjamin", "Love", "Lucas", "Lara", "Kevin", "Sten",
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	namePosition := rand.Intn(len(names))
+	randName := names[namePosition]
+	return randName
+}
